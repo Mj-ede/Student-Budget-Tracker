38b3494f9c4012acb365bb40e56e34c55521add5
Mj-ede <mede1@students.kennesaw.edu>
2025-11-24 18:17:03 -0500

    Differentiating b/w the example and starting my own code <3

---
+++ Mine_pastel_budget.py
@@ -0,0 +1,13 @@
+# So this will be the begining of the creating a aesthetic  budget tracker for everyone but students
+# for the colors I was thinking of using pastel colors to make it more visually appealing
+
+class Pastel:
+    PINK = "\033[95m"
+    MINT = "\033[96m"
+    LAVENDER = "\033[94m"
+    PEACH = "\033[91m"
+    SOFT_YELLOW = "\033[93m"
+    END = "\033[0m"
+    BOLD = "\033[1m"
+
+    
